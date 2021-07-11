58d5dc4a38db1f5e01394038d4ad1d76dc5dfca1
Hsuan Lee <boczeratul@gmail.com>
2021-07-11 20:01:41 +0800

    Add login controller

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import * as fcl from '@onflow/fcl'
+
+import Login from './components/Login'
 
 function App() {
+  const [address, setAddress] = useState(null)
+
+  useEffect(() => {
+    fcl.currentUser().subscribe(user => setAddress(user.addr))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login address={address} />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import * as fcl from '@onflow/fcl'
+
+const Login = ({ address }) => {
+    const loginOrLogout = () => {
+        if (address) {
+            fcl.unauthenticate()
+        } else {
+            fcl.authenticate()
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={loginOrLogout}>
+                {Boolean(address) ? 'Log out' : 'Log in'}
+            </button>
+
+            {address}
+        </div>
+    );
+}
+
+export default Login
